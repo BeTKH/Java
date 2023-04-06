07aa22852a9be854f35600d2f2d1d6913b52f919
Bekalu Tadesse <45674779+BeTKH@users.noreply.github.com>
2023-04-05 21:09:20 -0500

    Video Processor - decoupling via Interfaces example

---
+++ MoshOOP/LooseCoupling/DecouplingVideoProcessorExample/EmailService.java
@@ -0,0 +1,10 @@
+package MoshOOP.LooseCoupling.DecouplingVideoProcessorExample;
+
+public class EmailService {
+
+    public void sendEmail(User user) {
+        System.out.println("Notifying " + user.getEmail() + "...");
+        System.out.println("Done!\n");
+    }
+}
+
