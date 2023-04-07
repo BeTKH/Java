a56e4015a48fa0900e143e6fe5aa172c0cf788a3
Bekalu Tadesse <45674779+BeTKH@users.noreply.github.com>
2023-04-06 21:39:54 -0500

    exceptions demo

---
+++ MoshAdvancedJavaForEnterPriseApplications/Exceptions/ExceptionsDemo.java
@@ -0,0 +1,12 @@
+package MoshAdvancedJavaForEnterPriseApplications.Exceptions;
+
+import java.io.FileReader;
+
+public class ExceptionsDemo {
+
+    public static void show(){
+
+        var reader = new FileReader("file.txt");
+        System.out.println("File opened");
+    }
+}
