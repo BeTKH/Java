853361a32e66c72819061a962a1fd6039ec5b78f
Bekalu Tadesse <45674779+BeTKH@users.noreply.github.com>
2023-04-06 22:38:12 -0500

    alt + enter - surround with try-catch

---
+++ MoshAdvancedJavaForEnterPriseApplications/Exceptions/ExceptionMain.java
@@ -0,0 +1,10 @@
+package MoshAdvancedJavaForEnterPriseApplications.Exceptions;
+
+public class ExceptionMain {
+
+    public static void main(String[] args) {
+
+        ExceptionsDemo.show();
+
+    }
+}
--- MoshAdvancedJavaForEnterPriseApplications/Exceptions/ExceptionsDemo.java
+++ MoshAdvancedJavaForEnterPriseApplications/Exceptions/ExceptionsDemo.java
@@ -1,12 +1,21 @@
 package MoshAdvancedJavaForEnterPriseApplications.Exceptions;
 
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 
 public class ExceptionsDemo {
 
     public static void show(){
 
-        var reader = new FileReader("file.txt");
-        System.out.println("File opened");
+
+        // alt + enter - surround with try-catch
+
+        try {
+            var reader = new FileReader("file.txt");
+
+        } catch (FileNotFoundException e) {
+            e.getMessage();
+        }
+
     }
 }
