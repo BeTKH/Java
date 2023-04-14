6f9e16dae4260fd7f25f3f01248ac6e13e435366
Bekalu Tadesse <45674779+BeTKH@users.noreply.github.com>
2023-04-14 17:07:51 -0500

    bounded generic types

---
+++ MoshGenerics/ConstrainGenerics/BoundedGenericList.java
@@ -0,0 +1,20 @@
+package MoshGenerics.ConstrainGenerics;
+
+/**
+ * constraints (Bounded type parameters) are set using:
+ * - classes or
+ * - interfaces (-able)
+ * */
+public class BoundedGenericList<T extends Number & Comparable & Cloneable> {
+
+    private T[] items = (T[]) new Object[10];
+    private int count;
+
+    public void add(T item_){
+        items[count++] = item_;
+    }
+
+    public T get(int index_){
+        return items[index_];
+    }
+}
