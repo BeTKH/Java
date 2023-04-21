37ff1257b87461339b2f8aed86bc66d73e442875
Bekalu Tadesse <45674779+BeTKH@users.noreply.github.com>
2023-04-21 17:24:46 -0500

    genericlist class bug fix

--- AdvancedJavaTopics/MoshGenerics/sGoodMethodUsingGeneric/GenericList.java
+++ AdvancedJavaTopics/MoshGenerics/sGoodMethodUsingGeneric/GenericList.java
@@ -4,20 +4,38 @@ import java.util.Iterator;
 
 public class GenericList<T> implements Iterable<T>{
 
-    public T[] items = (T[]) new Object[10];
+    private T[] items = (T[]) new Object[10];
     private int count;
 
-    public void add(T item_){
-        items[count++] = item_;
+    public void add(T item) {
+        items[count++] = item;
     }
 
-    public T get(int index_){
-        return items[index_];
+    public T get(int index) {
+        return items[index];
     }
-
 
     @Override
     public Iterator<T> iterator() {
-        return null;
+        return new ListIterator(this);
+    }
+
+    private class ListIterator implements Iterator<T> {
+        private GenericList<T> list;
+        private int index;
+
+        public ListIterator(GenericList<T> list) {
+            this.list = list;
+        }
+
+        @Override
+        public boolean hasNext() {
+            return (index < list.count);
+        }
+
+        @Override
+        public T next() {
+            return list.items[index++];
+        }
     }
 }
