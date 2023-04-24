da6ae5b95b4d6fbec3bd410dbb5e80af60ef8287
Bekalu Tadesse <45674779+BeTKH@users.noreply.github.com>
2023-04-24 17:13:20 -0500

    string concat in java

---
+++ StringsIO/StringClass/Concatination.java
@@ -0,0 +1,13 @@
+package StringsIO.StringClass;
+
+public class Concatination {
+
+    public static void main(String[] args) {
+
+
+        String s1 = "hello";
+        String s2 = "_world";
+
+        System.out.println(s1.concat(s2).toUpperCase());
+    }
+}
