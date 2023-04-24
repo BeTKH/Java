cbae53a99c0f3889bafc887865501f6169f341b2
Bekalu Tadesse <45674779+BeTKH@users.noreply.github.com>
2023-04-24 17:09:33 -0500

    string comparison

--- StringsIO/StringClass/StringComparison.java
+++ StringsIO/StringClass/StringComparison.java
@@ -1,4 +1,21 @@
 package StringsIO.StringClass;
 
 public class StringComparison {
+
+
+    public static void main(String[] args) {
+
+        String s1 = "I am one";
+        String s2 = "I am two";
+
+
+
+        System.out.println(s1.equals(s2));
+        System.out.println(s1.compareTo(s2));
+        System.out.println(s1.length());
+        System.out.println(s1.charAt(3));
+        System.out.println(s1.codePointAt(3));
+    }
+
+
 }
