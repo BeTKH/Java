d91320c31f027c2cd736610f77f539a9d50f13e6
Bekalu Tadesse <45674779+BeTKH@users.noreply.github.com>
2023-04-24 17:10:06 -0500

    ways to create strings in java

--- StringsIO/StringsMain.java
+++ StringsIO/StringsMain.java
@@ -1,4 +1,20 @@
 package StringsIO;
 
 public class StringsMain {
+
+    public static void main(String[] args) {
+
+
+        String newStr = new String("I am string literal");
+        System.out.println(newStr);
+
+
+        String newStr2 = "String with a variable";
+        System.out.println(newStr2);
+
+        char[] charArray = {'G', 'o', 'o', 'f','y'};
+        String newString3 = new String(charArray);
+        System.out.println(newString3);
+
+    }
 }
