a88a068d225b2dda68fb8e8c78f24b3ba65fbf1a
Bekalu Tadesse <45674779+BeTKH@users.noreply.github.com>
2023-04-24 17:16:20 -0500

    trim string

--- StringsIO/StringClass/Concatination.java
+++ StringsIO/StringClass/Concatination.java
@@ -5,9 +5,9 @@ public class Concatination {
     public static void main(String[] args) {
 
 
-        String s1 = "hello";
-        String s2 = "_world";
+        String s1 = "          hello";
+        String s2 = "_world  !";
 
-        System.out.println(s1.concat(s2).toUpperCase());
+        System.out.println(s1.concat(s2).toUpperCase().trim());
     }
 }
